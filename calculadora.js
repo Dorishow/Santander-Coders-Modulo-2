c5b75e3ce1798c8feb2f4d73ab94778f7c838857
João Almeida <69915069+Dorishow@users.noreply.github.com>
2021-10-12 00:46:12 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,60 +1,62 @@
-    const calculadora = (() =>{let calc = {n1: 0, operator: '', n2: 0}
-    let EnterController = 'n1'
-    let history = []
+    const calculadora = (() =>{let calc = {
+        
+        n1: 0, operator: '', n2: 0}
+        let EnterController = 'n1'
+        let history = []
 
-    const isNumber = (n) => typeof n == 'number'
-    const isString = (n) => typeof n == 'string'
+        const isNumber = (n) => typeof n == 'number'
+        const isString = (n) => typeof n == 'string'
 
-    const isFirstNumber = (EnterController) => EnterController === 'n1'
-    const isOperator = (EnterController) => EnterController === 'operator'
-    const isSecondNumber = (EnterController) => EnterController === 'n2'
+        const isFirstNumber = (EnterController) => EnterController === 'n1'
+        const isOperator = (EnterController) => EnterController === 'operator'
+        const isSecondNumber = (EnterController) => EnterController === 'n2'
 
-    const setToCalculator = (prop, data) => calc = {...calc, [prop]: data}
-    const nextEnterWillBe = (value) => EnterController = value
+        const setToCalculator = (prop, data) => calc = {...calc, [prop]: data}
+        const nextEnterWillBe = (value) => EnterController = value
 
-    function enter(data){
-        if(isNumber(data)){
-            if(isFirstNumber(EnterController)){
-                setToCalculator('n1', data)
-                nextEnterWillBe('operator')
+        function enter(data){
+            if(isNumber(data)){
+                if(isFirstNumber(EnterController)){
+                    setToCalculator('n1', data)
+                    nextEnterWillBe('operator')
+                }
+                else if(isSecondNumber(EnterController)){
+                    setToCalculator('n2', data)
+                }
+                else{nextEnterWillBe('n1')}
             }
-            else if(isSecondNumber(EnterController)){
-                setToCalculator('n2', data)
+            else if(isString(data)){
+                if(isOperator(EnterController)){
+                    setToCalculator('operator', data)
+                    nextEnterWillBe('n2')
+                }
             }
-            else{nextEnterWillBe('n1')}
+            else{console.log('caractere inválido')}
         }
-        else if(isString(data)){
-            if(isOperator(EnterController)){
-                setToCalculator('operator', data)
-                nextEnterWillBe('n2')
+    
+        function equals(){
+            if(EnterController === 'n2'){
+                history = [...history, {...calc, resultado: calculate(calc)}]
+                nextEnterWillBe('n1')
+                return console.log(calculate(calc))
             }
         }
-        else{console.log('caractere inválido')}
-    }
+
+        const list = () => console.log(history)
+
+        const reset = () => history = []
     
-    function equals(){
-        if(EnterController === 'n2'){
-            history = [...history, {...calc, resultado: calculate(calc)}]
-            nextEnterWillBe('n1')
-            return console.log(calculate(calc))
+        function calculate({n1, operator, n2}){
+            if(operator === '+') return n1 + n2
+            if(operator === '-') return n1 - n2
+            if(operator === '*') return n1 * n2
+            if(operator === '/') return n1 / n2
+            else return 'operator inválido'
         }
-    }
 
-    const list = () => console.log(history)
-
-    const reset = () => history = []
-    
-    function calculate({n1, operator, n2}){
-        if(operator === '+') return n1 + n2
-        if(operator === '-') return n1 - n2
-        if(operator === '*') return n1 * n2
-        if(operator === '/') return n1 / n2
-        else return 'operator inválido'
-    }
-    
-    return {
-        enter, equals, list, reset
-    }
+        return {
+            enter, equals, list, reset
+        }
 
 })()
 
